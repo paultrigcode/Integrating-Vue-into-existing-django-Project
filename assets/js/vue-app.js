5a73707679af174cf3c606a5aead1b601619200c
Ajakaye <mailpaulade@gmail.com>
2021-01-15 15:32:56 +0100

    some changes--tried to change the text it is displaying in the example component but there wasnt any changes

--- assets/js/vue-app.js
+++ assets/js/vue-app.js
@@ -2471,13 +2471,11 @@ var staticRenderFns = [
       _c("div", { staticClass: "row justify-content-center" }, [
         _c("div", { staticClass: "col-md-8" }, [
           _c("div", { staticClass: "card" }, [
-            _c("div", { staticClass: "card-header" }, [
-              _vm._v("Example Component")
-            ]),
+            _c("div", [_vm._v("Example Component")]),
             _vm._v(" "),
-            _c("div", { staticClass: "card-body" }, [
+            _c("div", [
               _vm._v(
-                " \n                    I'm an example component. \n                "
+                " \n                    I'm an example component yes ia m oooooooooo0000ooo. \n                "
               )
             ])
           ])
--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -3,9 +3,9 @@
         <div class="row justify-content-center"> 
             <div class="col-md-8"> 
                 <div class="card"> 
-                    <div class="card-header">Example Component</div> 
-                    <div class="card-body"> 
-                        I'm an example component. 
+                    <div>Example Component</div> 
+                    <div> 
+                        I'm an example component yes ia m oooooooooo0000ooo. 
                     </div> 
                 </div> 
             </div> 
