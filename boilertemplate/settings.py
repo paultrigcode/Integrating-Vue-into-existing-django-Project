653e8fa9b267d43ca02e25bd2775a00ead200e21
Ajakaye <mailpaulade@gmail.com>
2021-01-17 14:02:22 +0100

    changed db settings and gitignore to include db.sqlite

--- boilertemplate/settings.py
+++ boilertemplate/settings.py
@@ -76,23 +76,23 @@ WSGI_APPLICATION = 'boilertemplate.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': os.getenv("DB_NAME","django-static"),                      # Or path to database file if using sqlite3.
-        'USER': os.getenv("DB_USER", "root"),
-        'PASSWORD': os.getenv("DB_PASS", "root"),
-        'HOST': os.getenv("DB_HOST", "localhost"),
-        'PORT': os.getenv("DB_PORT", "3306"),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+#         'NAME': os.getenv("DB_NAME","django-static"),                      # Or path to database file if using sqlite3.
+#         'USER': os.getenv("DB_USER", "root"),
+#         'PASSWORD': os.getenv("DB_PASS", "root"),
+#         'HOST': os.getenv("DB_HOST", "localhost"),
+#         'PORT': os.getenv("DB_PORT", "3306"),
+#     }
+# }
 
 
 
