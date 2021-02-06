e8aee022a38b80c04533f4f0dbaf814482ad6131
Ajakaye <mailpaulade@gmail.com>
2021-02-06 20:09:50 +0100

    completed docker setup on project

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ echo 'Starting python process'
 # watch -n 3 "wget 0.0.0.0:8000 -O /dev/null -o /dev/null" &
 echo 'yes'| python manage.py collectstatic 
 
-/usr/local/bin/gunicorn we_sms.wsgi:application -w 2 -b :9000 --reload --reload-extra-file="/src/."
+/usr/local/bin/gunicorn boilertemplate.wsgi:application -w 2 -b :9000 --reload --reload-extra-file="/src/."
